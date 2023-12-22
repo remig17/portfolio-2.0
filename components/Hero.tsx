b437d7b2fa6181ff6f2874fbbe8ac639ca3f8671
Remi <remigallois@gmail.com>
2023-12-22 09:04:49 +0100

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+type Props = {};
+
+export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, my name is Rémi",
+      "I-Love-Learning-New-Stuff.tsx",
+      "<CodingIsAGameChanger/>",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor="5F00BA" />
+      </h1>
+    </div>
+  );
+}
