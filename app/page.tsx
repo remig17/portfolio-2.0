c289df9a1553b695dffc2db78a699d96f2a5fc2f
Remi <remigallois@gmail.com>
2024-01-02 12:30:58 +0100

    correcting type error of Home export

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ type Props = {
   socials?: Social[];
 };
 
-export default function Home({ experiences }: Props) {
+const Home: React.FC<Props> = ({ experiences }) => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#5F00BA]/80">
       <Head>
@@ -60,4 +60,6 @@ export default function Home({ experiences }: Props) {
       </Link>
     </div>
   );
-}
+};
+
+export default Home;
